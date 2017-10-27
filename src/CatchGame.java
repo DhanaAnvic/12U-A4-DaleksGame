cd5e1f80448cf765739f4d90ce467095cfefc3ff
fabed2976 <fabed2976@HRH-IC0027295.wrdsb.ca>
2017-10-27 14:08:40 -0400

    kinda Finished but not working

--- src/CatchGame.java
+++ src/CatchGame.java
@@ -18,6 +18,7 @@ public class CatchGame {
     private Doctor doctor;
     private Dalek dalek1, dalek2, dalek3;
     private Board board;
+
     
   
     /**
@@ -27,26 +28,36 @@ public class CatchGame {
      */
     public CatchGame(){
       //create the game board
-     Board board = new Board(12,12);
-   
+      board = new Board(12,12);
+      
+      int docRow = (int)(Math.random() *12);
+      int docCol = (int) (Math.random() *12);
       //create the doctor
-      Doctor doctor = new Doctor ((int)(Math.random() * 12), (int) (Math.random() * 12));
+       doctor = new Doctor (docRow, docCol);
+      //putting a green peg for the doctor
+     board.putPeg(Color.GREEN,doctor.getRow(), doctor.getCol());
 
+     int d1Row = (int)(Math.random() * 12);
+     int d1Col= (int)(Math.random() * 12);
      //create the first dalek
-    Dalek  dalek1 = new Dalek((int)(Math.random() * 12), (int) (Math.random() * 12));
+      dalek1 = new Dalek(d1Row, d1Col);
+       //putting a black peg for the dalek1
+     board.putPeg(Color.BLACK,dalek1.getRow(), dalek1.getCol());
 
+     int d2Row = (int)(Math.random() * 12);
+     int d2Col= (int)(Math.random() * 12);
      //create the second dalek
-     Dalek dalek2 = new Dalek ((int)(Math.random() * 12), (int) (Math.random() * 12));
+     dalek2 = new Dalek (d2Row, d2Col);
+     //putting a black peg for the dalek2
+     board.putPeg(Color.BLACK, d2Row, d2Col);
  
+     int d3Row = (int)(Math.random() * 12);
+     int d3Col= (int)(Math.random() * 12);
      //create the third dalek
-     Dalek dalek3 = new Dalek ((int)(Math.random() * 12), (int) (Math.random() * 12));
-     
-     board.putPeg(Color.GREEN, doctor.getRow(), doctor.getCol());
-     board.putPeg(Color.BLACK, dalek1.getRow(), dalek1.getCol());
-     board.putPeg(Color.BLACK, dalek2.getRow(), dalek2.getCol());
-     board.putPeg(Color.BLACK, dalek3.getRow(), dalek3.getCol());
-
-     
+    dalek3 = new Dalek (d3Row, d3Col);
+     //putting a black peg for the dalek3
+    board.putPeg(Color.BLACK, d3Col, d3Row);
+ 
     }
  
     /**
@@ -58,43 +69,90 @@ public class CatchGame {
         while(true){
         //determine the user's doctor move
         Coordinate click = board.getClick();
+
          //Remove the old doctor peg
           board.removePeg(doctor.getRow(), doctor.getCol());
-          board.removePeg(dalek1.getRow(), dalek1.getCol());
-          board.removePeg(dalek2.getRow(), dalek2.getCol());
-          board.removePeg(dalek3.getRow(), dalek3.getCol());
+          //remove the old three daleks peg
+        board.removePeg(dalek1.getRow(), dalek1.getCol());
+        board.removePeg(dalek2.getRow(), dalek2.getCol());
+        board.removePeg(dalek3.getRow(), dalek3.getCol());
           
           // Move the doctor peg to new location where the player clicked
           doctor.move(click.getRow(), click.getCol());
+          //all the dalek will go foward to the doctor
           dalek1.advanceTowards(doctor);
           dalek2.advanceTowards(doctor);
           dalek3.advanceTowards(doctor);
-          
-          
+   
           // Place the doctor peg!
           board.putPeg(Color.GREEN, doctor.getRow(), doctor.getCol());
-          board.putPeg(Color.BLACK, dalek1.getRow(), dalek1.getCol());
+          
+          //place the three dalek pegs
+          board.putPeg(Color.BLUE, dalek1.getRow(), dalek1.getCol());
           board.putPeg(Color.BLACK, dalek2.getRow(), dalek2.getCol());
           board.putPeg(Color.BLACK, dalek3.getRow(), dalek3.getCol());
           
           
-          //check if all the daleks crash
-          if(doctor.getRow() == dalek1.getRow() && doctor.getCol() == dalek1.getRow()){
+          //check if each dalek 1 will crash to the doctor
+          if (doctor.getRow() == dalek1.getRow() && doctor.getCol() == dalek1.getRow()){
               board.removePeg(dalek1.getRow(), dalek1.getCol());
               board.removePeg(doctor.getRow(), doctor.getCol());
-              board.putPeg(Color.YELLOW, doctor.getRow(), doctor.getCol());
-
+              board.putPeg(Color.YELLOW, doctor.getRow(), doctor.getCol()); 
+              
+           //check if dalek 2 will collide to the doctor
           }
-          
-
-
+         if(doctor.getRow() == dalek2.getRow() && doctor.getCol() == dalek2.getCol()){
+              board.removePeg(dalek2.getRow(), dalek2.getCol());
+              board.removePeg(doctor.getRow(), doctor.getCol());
+              board.putPeg(Color.YELLOW, doctor.getRow(), doctor.getCol());
+              
+          }
+       
+        if(doctor.getRow() == dalek3.getRow() && doctor.getCol() == dalek3.getCol()){
+              board.removePeg(dalek3.getRow(), dalek3.getCol());
+              board.removePeg(doctor.getRow(), doctor.getCol());
+              board.putPeg(Color.YELLOW, doctor.getRow(), doctor.getCol());
+              break;
+          }
+        }
+          //check if each dalek crash each other
+         if (dalek1.getRow() == dalek2.getRow() && dalek1.getRow() == dalek2.getRow()){
+              board.removePeg(dalek1.getRow(), dalek1.getCol());
+              board.removePeg(dalek2.getRow(), dalek2.getCol());
+              board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+              dalek1.crash();
+              dalek2.crash();
+              
+          }
+         if(dalek1.getRow() == dalek3.getRow() && dalek1.getCol() == dalek3.getCol()){
+              board.removePeg(dalek1.getRow(), dalek1.getCol());
+              board.removePeg(dalek3.getRow(), dalek3.getCol());
+              board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+              dalek1.crash();
+              dalek3.crash();
+              
+          }
+        if(dalek2.getRow() == dalek3.getRow() && dalek2.getCol() == dalek3.getCol()){
+              board.removePeg(dalek2.getRow(), dalek2.getCol());
+              board.removePeg(dalek3.getRow(), dalek3.getCol());
+              board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+              dalek2.crash();
+              dalek3.crash();
     }
-}
-}
+    if(dalek1.getRow() == dalek2.getRow() && dalek1.getRow() == dalek3.getRow() 
+            && dalek1.getCol() == dalek2.getCol() && dalek1.getCol() == dalek3.getCol() &&
+            dalek2.getRow() == dalek3.getRow() && dalek2.getCol() == dalek3.getRow()){
+        dalek1.crash();;
+        dalek2.crash();
+        dalek3.crash();
+        board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+        board.displayMessage("You have lost!!");
 
+       
+    }
         
-
-
+        }
+    }
 
 
 
